c61cf62e20770695567eb75beb20aa2bfafd0088
David <maroon127@gmail.com>
2016-05-18 16:02:29 -0400

    cleanup

--- build-mega-atmega2560/core/WMath.d
+++
@@ -1,2 +0,0 @@
-build-mega-atmega2560/core/WMath.o: \
- /usr/share/arduino/hardware/arduino/avr/cores/arduino/WMath.cpp
--- build-mega-atmega2560/libs/Servo/src/sam/Servo.d
+++
@@ -1,2 +0,0 @@
-build-mega-atmega2560/libs/Servo/src/sam/Servo.o: \
- /usr/share/arduino/libraries/Servo/src/sam/Servo.cpp
