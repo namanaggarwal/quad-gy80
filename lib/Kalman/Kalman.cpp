51ae1d20db3c912286d239847f9f8cad0940ff01
David <ec2-user@ip-172-31-42-44.us-east-2.compute.internal>
2018-02-27 22:50:05 +0000

    attempting 3D kalman filters

--- lib/Kalman/Kalman.cpp
+++ lib/Kalman/Kalman.cpp
@@ -1,11 +1,25 @@
-#include "Filter.h"
+/*************************************************************************
+* 
+*  This Kalman filter predicts the next state whenever PredictNextState is
+*  is called. It keeps track of its own process covariance matrix and its
+*  process and observational error. The class binds to two sensor objects
+*  and reports the predicted state for 3 axis. Two instances of this class
+*  are needed to cover the six axis required for movement in a 3-D space.
+*   
+*************************************************************************/
+
+#include "Kalman.h"
 
 
 
-Filter::Filter(Sensor* sen_a, Sensor* sen_b) {
-  // filter needs to acquire data at every iteration in order
+Kalman::Kalman(L3G4200D* gyro, ADXL345* accel) {
+
+  
 
 }
+
+
+
 
 
 
